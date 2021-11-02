a2cc28e0b39399f4c76eb71f317c7258a2232451
Taraneh N <taranehn@gmail.com>
2021-11-02 21:51:30 +0100

    links to social media

--- src/Pages/AboutUs.tsx
+++ src/Pages/AboutUs.tsx
@@ -1,10 +1,23 @@
-import { Grid, Typography } from '@material-ui/core'
+import { Grid, IconButton, Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
-import { useIntl } from 'react-intl'
+import { useIntl } from 'react-intl';
+import Facebook from '@mui/icons-material/Facebook';
+import Instagram from '@mui/icons-material/Instagram';
+import YouTube from '@mui/icons-material/YouTube';
+import Telegram from '@mui/icons-material/Telegram';
+import { PATOGH_INSTAGRAM, PATOGH_FACEBOOK, PATOGH_YOUTUBE, PATOGH_TELEGRAM, SEEM_INSTAGRAM } from '../const/links';
 
 const useStyles = makeStyles((theme) => ({
     root: {
         padding: theme.spacing(2, 4),
+    },
+    icon: {
+        color: theme.palette.secondary.main,
+        padding: theme.spacing(0.5),
+    },
+    title: {
+        margin: 0,
+        padding: theme.spacing(1, 1, 1, 0),
     }
 }));
 
@@ -25,17 +38,70 @@ export default function AboutUs({ image }: ItemProps) {
             alignItems="flex-start"
         >
             <Grid item>
-                <Typography color="textSecondary" gutterBottom variant="h5">
-                    {intl.formatMessage({ id: "seem-festival" })}
-                </Typography>
+                <Grid
+                    container
+                    direction="row"
+                    justifyContent="flex-start"
+                    spacing={0}
+                    alignItems="center"
+                >
+                    <Grid item>
+                        <Typography className={classes.title}
+                            color="textSecondary" gutterBottom variant="h5">
+                            {intl.formatMessage({ id: "seem-festival" })}
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <IconButton className={classes.icon}
+                            href={SEEM_INSTAGRAM} target="_blank"  >
+                            <Instagram />
+                        </IconButton>
+                    </Grid>
+                </Grid>
                 <Typography color="textPrimary" gutterBottom >
                     {intl.formatMessage({ id: "seem-des" })}
                 </Typography>
             </Grid>
+
             <Grid item>
-                <Typography color="textSecondary" gutterBottom variant="h5">
-                    {intl.formatMessage({ id: "patogh" })}
-                </Typography>
+                <Grid
+                    container
+                    direction="row"
+                    justifyContent="flex-start"
+                    spacing={0}
+                    alignItems="center"
+                >
+                    <Grid item>
+                        <Typography className={classes.title} color="textSecondary" gutterBottom variant="h5">
+                            {intl.formatMessage({ id: "patogh" })}
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <IconButton className={classes.icon}
+                            href={PATOGH_FACEBOOK} target="_blank"  >
+                            <Facebook />
+                        </IconButton>
+                    </Grid>
+                    <Grid item>
+                        <IconButton className={classes.icon}
+                            href={PATOGH_INSTAGRAM} target="_blank"  >
+                            <Instagram />
+                        </IconButton>
+                    </Grid>
+                    <Grid item>
+                        <IconButton className={classes.icon}
+                            href={PATOGH_YOUTUBE} target="_blank"  >
+                            <YouTube />
+                        </IconButton>
+                    </Grid>
+                    <Grid item>
+                        <IconButton className={classes.icon}
+                            href={PATOGH_TELEGRAM} target="_blank"  >
+                            <Telegram />
+                        </IconButton>
+                    </Grid>
+                </Grid>
+
                 <Typography color="textPrimary" gutterBottom >
                     {intl.formatMessage({ id: "patogh-des" })}
                 </Typography>
