a150e4cff1b59f7492e88e09f290e2056f6513ec
Taraneh N <taranehn@gmail.com>
2021-11-08 19:01:37 +0100

    Spotify + Soundcloud icons

--- src/artists/Makan.tsx
+++ src/artists/Makan.tsx
@@ -6,6 +6,7 @@ import { MAKAN_WEBSITE, MAKAN_INSTAGRAM, MAKAN_SOUNDCOUD } from '../const/artist
 import YouTube from 'react-youtube';
 import Instagram from '@mui/icons-material/Instagram';
 import { ReactComponent as Soundcloud } from '../icons/soundcloud.svg';
+import { ReactComponent as Spotify } from '../icons/spotify.svg';
 
 const useStyles = makeStyles((theme) => ({
   title: {
@@ -16,12 +17,17 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.secondary.main,
     padding: theme.spacing(0, 0.5, 0.5, 0),
   },
-  svg: {
-    fill: theme.palette.secondary.main,
-    margin: theme.spacing(1, 0, 0.5, 0),
-    width: '24px',
-    height: '24px'
+  spotify: {
+    margin: theme.spacing(0.5, 0, 0, 0.25),
+    padding: theme.spacing(0, 0.5, 0.5, 0),
+    width: '25px',
+    height: '25px'
   },
+  soundcoud: {
+    padding: theme.spacing(0),
+    width: '25px',
+    height: '25px'
+  }
 }));
 
 export default function Persian() {
@@ -73,16 +79,16 @@ export default function Persian() {
             <Grid item>
               <IconButton className={classes.icon}
                 href={MAKAN_INSTAGRAM} target="_blank"  >
-                <SvgIcon className={classes.svg}  >
-                  <Soundcloud />
+                <SvgIcon className={classes.spotify} >
+                  < Spotify />
                 </SvgIcon>
               </IconButton>
             </Grid>
             <Grid item>
-              <IconButton
+              <IconButton className={classes.icon}
                 href={MAKAN_SOUNDCOUD} target="_blank"   >
-                <SvgIcon className={classes.svg}  >
-                  <path d="M 30 11 C 26.398438 11 23 12.789063 21 15.6875 L 21 36 L 42.5 36 C 46.601563 36 50 32.601563 50 28.5 C 50 24.398438 46.601563 21 42.5 21 C 42 21 41.5 21.085938 41 21.1875 C 40.5 15.488281 35.800781 11 30 11 Z M 17 16 C 16.300781 16 15.601563 16.085938 15 16.1875 L 15 36 L 17 36 Z M 18 16 L 18 36 L 20 36 L 20 16.5 C 19.398438 16.300781 18.699219 16.101563 18 16 Z M 14 16.5 C 13.300781 16.800781 12.601563 17.101563 12 17.5 L 12 36 L 14 36 Z M 11 18.3125 C 10.199219 19.011719 9.5 19.90625 9 20.90625 L 9 36 L 11 36 Z M 6.5 22 C 6.324219 22.011719 6.148438 22.042969 6 22.09375 L 6 35.90625 C 6.300781 36.007813 6.699219 36 7 36 L 8 36 L 8 22.09375 C 7.699219 21.992188 7.300781 22 7 22 C 6.851563 22 6.675781 21.988281 6.5 22 Z M 5 22.3125 C 4.300781 22.511719 3.601563 22.8125 3 23.3125 L 3 34.6875 C 3.601563 35.085938 4.300781 35.488281 5 35.6875 Z M 2 24.09375 C 0.800781 25.394531 0 27.101563 0 29 C 0 30.898438 0.800781 32.605469 2 33.90625 Z" />
+                <SvgIcon className={classes.soundcoud} >
+                  < Soundcloud />
                 </SvgIcon>
               </IconButton>
             </Grid>
